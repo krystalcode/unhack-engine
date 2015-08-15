1e4208c89be21eef9435b218ddd00806107a2f25
Dimitris Bozelos <dbozelos@gmail.com>
2015-08-15 10:00:31 +0000

    updated main module fileto include submodules

--- src/Unhack.hs
+++ src/Unhack.hs
@@ -1,5 +1,11 @@
 module Unhack
        ( module Unhack.Parser
+       , module Unhack.Project
+       , module Unhack.Issue
+       , module Unhack.ElasticSearch
        ) where
 
 import Unhack.Parser
+import Unhack.Project
+import Unhack.Issue
+import Unhack.ElasticSearch
