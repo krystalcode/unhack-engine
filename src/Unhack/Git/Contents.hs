65042feee10ae6c0ff4431fd3ed272b394f722d5
Dimitris Bozelos <dbozelos@gmail.com>
2016-01-20 03:21:42 +0000

    use tuples in alternate function for getting file contents from git

--- src/Unhack/Git/Contents.hs
+++ src/Unhack/Git/Contents.hs
@@ -17,7 +17,7 @@ fileContents :: FilePath -> Commit -> T.Text -> IO (String)
 fileContents directory commit file = strictProcess command directory
     where command = "git show " ++ (T.unpack $ hash commit) ++ ":" ++ (T.unpack file)
 
-fileContents' :: FilePath -> Commit -> T.Text -> IO (Commit, String)
-fileContents' directory commit file = do
+fileContents' :: FilePath -> (Commit, T.Text) -> IO (Commit, T.Text, String)
+fileContents' directory (commit, file) = do
     contents <- fileContents directory commit file
-    return (commit, contents)
+    return (commit, file, contents)
