19e0449c794e0c0a66c198591444d9e309a2dfb7
Dimitris Bozelos <dbozelos@gmail.com>
2016-04-01 16:33:53 +0000

    Added functions for extracting information from a repository's config file
     - Get the vendor that a repository is hosted on.
     - Get the name of the repository.
     - Convert the url of a repository to its name or vendor.

--- src/Unhack/Git/Config.hs
+++ src/Unhack/Git/Config.hs
@@ -1,11 +1,61 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Unhack.Git.Config ( remoteUrl ) where
+module Unhack.Git.Config
+       ( remoteUrl
+       , remoteName
+       , remoteVendor
+       , remoteUrlToName
+       , remoteUrlToVendor ) where
 
-import qualified Data.Text as T (Text)
+
+-- Imports.
+
+import qualified Data.Text as T (pack, replace, Text)
 import Unhack.Process
 
 -- Public API
 
+{-
+    @Issue(
+        "The Unhack.Git.Config module may not be needed anymore with the new
+        flow of tasks"
+        type="improvement"
+        priority="low"
+        labels="cleanup"
+    )
+-}
+
 remoteUrl :: FilePath -> IO (T.Text)
 remoteUrl directory = lazyProcess "git config --get remote.origin.url" directory
+
+remoteName :: FilePath -> IO (T.Text)
+remoteName directory = do
+    url <- remoteUrl directory
+    return $ remoteUrlToName url
+
+remoteVendor :: FilePath -> IO (T.Text)
+remoteVendor directory = do
+    url <- remoteUrl directory
+    return $ remoteUrlToVendor url
+
+{-
+    @Issue(
+        "Improve the reliability of this function and support other vendors and protocols"
+        type="bug"
+        priority="low"
+    )
+-}
+remoteUrlToName :: T.Text -> T.Text
+remoteUrlToName url = withExtensionRemoved
+    where withVendorParsed     = T.replace "git@bitbucket.org:" "bitbucket" url
+          withExtensionRemoved = T.replace ".git" "" url
+
+{-
+    @Issue(
+        "Implement this function with support for multiple vendors"
+        type="bug"
+        priority="low"
+    )
+-}
+remoteUrlToVendor :: T.Text -> T.Text
+remoteUrlToVendor url = T.pack "bitbucket"
