366952d7261906edae44337c235a04971dc8e632
Dimitris Bozelos <dbozelos@gmail.com>
2016-04-01 16:13:34 +0000

    Added functionality for clonging a git repository

--- src/Unhack/Git/Fetch.hs
+++ src/Unhack/Git/Fetch.hs
@@ -1,13 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Unhack.Git.Fetch
-       ( fetch
+       ( clone
+       , fetch
        , fetchBranch ) where
 
-import qualified Data.Text as T (concat, Text)
+
+-- Imports.
+
+import Control.Monad (unless)
+import qualified Data.Text as T (concat, unpack, Text)
+import System.Directory (createDirectoryIfMissing, doesDirectoryExist)
 import Unhack.Process
 
 -- Public API
+
+{-
+    @Issue(
+        "Add a function that does a clone or fetch depending on whether the
+        repository is already cloned"
+        type="improvement"
+        priority="low"
+    )
+-}
+
+clone :: T.Text -> T.Text -> T.Text -> IO (T.Text)
+clone vendor owner repository = do
+    createDirectoryIfMissing True directory
+    lazyProcess command directory
+    where command   = T.concat ["git clone --no-checkout ", url, " ", repository]
+          directory = T.unpack $ T.concat ["/repositories/", vendor, "/", owner]
+          url       = case vendor of
+
+                          "bitbucket" -> T.concat ["git@bitbucket.org:", owner, "/", repository, ".git"]
+
+                          _           -> error $ "Unsupported vendor '" ++ (T.unpack vendor) ++ "' while cloning the repository '" ++ (T.unpack repository)
 
 {-
     @Issue(
