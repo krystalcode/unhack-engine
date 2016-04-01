f5066de546da07ed5b3d1dbf9f7e236e8b253c29
Dimitris Bozelos <dbozelos@gmail.com>
2016-04-01 03:47:29 +0000

    Added record definition for embedded commits

---
+++ src/Unhack/Data/EmCommit.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+
+module Unhack.Data.EmCommit
+       ( emptyEmCommit
+       , toCommits
+       , EmCommit(..)
+       ) where
+
+
+-- Imports.
+
+import Data.Aeson
+import Data.Aeson.Types (typeMismatch)
+import qualified Data.Text as T (Text)
+import GHC.Generics (Generic)
+import qualified Unhack.Commit as UC
+
+
+-- Public API.
+
+data EmCommit = EmCommit
+    { _id          :: T.Text
+    , hash         :: T.Text
+    , time         :: T.Text
+    , buildStatus  :: T.Text
+    , buildMessage :: T.Text } deriving (Generic, Show)
+
+emptyEmCommit :: EmCommit
+emptyEmCommit = EmCommit
+    { _id          = ""
+    , hash         = ""
+    , time         = ""
+    , buildStatus  = ""
+    , buildMessage = ""}
+
+toCommits :: [EmCommit] -> T.Text -> [UC.Commit]
+toCommits emCommits repositoryId = map mapCommit emCommits
+    where mapCommit emCommit = UC.emptyCommit { UC.repositoryId = repositoryId
+                                              , UC.hash         = hash emCommit
+                                              , UC.time         = time emCommit
+                                              , UC.buildStatus  = buildStatus emCommit
+                                              , UC.buildMessage = buildMessage emCommit }
+
+
+-- Functions/types for internal use.
+
+instance FromJSON EmCommit where
+    parseJSON (Object v) = EmCommit
+                           <$> v .: "_id"
+                           <*> v .: "hash"
+                           <*> v .: "time"
+                           <*> v .: "buildStatus"
+                           <*> v .: "buildMessage"
+    parseJSON invalid    = typeMismatch "EmCommit" invalid
+
+instance ToJSON EmCommit where
+    toJSON (EmCommit _id hash time buildStatus buildMessage) =
+        object [ "_id"          .= _id
+               , "hash"         .= hash
+               , "time"         .= time
+               , "buildStatus"  .= buildStatus
+               , "buildMessage" .= buildMessage ]
