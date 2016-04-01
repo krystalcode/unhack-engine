57642f5d17fadec090a896cc521d583cc175e37f
Dimitris Bozelos <dbozelos@gmail.com>
2016-04-01 16:54:16 +0000

    Changed the way config is passed on by the pubsub dispatcher

--- src/Unhack/Pubsub/Dispatcher.hs
+++ src/Unhack/Pubsub/Dispatcher.hs
@@ -8,6 +8,7 @@ import qualified Data.ByteString.Char8 as BS (split, unpack, ByteString)
 import qualified Data.Text as T (pack)
 import qualified Unhack.Pubsub.Repository as UPR
 import qualified Unhack.Storage.ElasticSearch.Config as USEC (StorageConfig, StorageIndexSettings)
+import qualified Unhack.Storage.ElasticSearch.Config as USEC (indexSettingsFromConfig, StorageConfig, StorageIndexSettings)
 
 
 -- Public API.
@@ -27,8 +28,8 @@ import qualified Unhack.Storage.ElasticSearch.Config as USEC (StorageConfig, Sto
         labels="pubsub"
     )
 -}
-dispatch :: USEC.StorageConfig -> USEC.StorageIndexSettings -> BS.ByteString -> IO ()
-dispatch config indexSettings message = do
+dispatch :: USEC.StorageConfig -> BS.ByteString -> IO ()
+dispatch config message = do
     let messageParts = decodeMessage message
     let action       = BS.unpack $ messageParts !! 0
 
@@ -38,7 +39,7 @@ dispatch config indexSettings message = do
         "repositories_create" -> do
             let repositoryId = T.pack (BS.unpack $ messageParts !! 1)
             print $ "Dispatching message of type '" ++ action ++ "'"
-            UPR.clone config indexSettings repositoryId
+            UPR.clone config (USEC.indexSettingsFromConfig "repository" config) repositoryId
             {-
                 @Issue(
                     "Execute the remaining tasks according to the flow"
